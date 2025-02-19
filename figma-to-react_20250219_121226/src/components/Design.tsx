ddb111fc5d2f18ea9daf1135e009344b50b0b103
Aparna-Promptora <babyaparna31@gmail.com>
2025-02-19 12:13:35 +0530

    Add/Update React project in dedicated folder

---
+++ figma-to-react_20250219_121226/src/components/Design.tsx
@@ -0,0 +1,77 @@
+import { React } from 'react';
+import styles from '../styles/Design.module.css';
+
+
+const Design: React.FC = () => {
+    
+    return (
+        <>
+            
+<div className={(styles['class-0']) + " class=DESIGN"} figma_id="11:10">
+<div className={(styles['class-1']) + " class=Rectangle398960"} figma_id="11:11">
+</div>
+<div className={(styles['class-2']) + " class=Rectangle398954"} figma_id="11:12">
+</div><div className={(styles['class-3']) + " class=EVERNORTH"} figma_id="11:13">EVERNORTH</div><div className={(styles['class-4']) + " class=PROVIDERDATAMANAGEMENT"} figma_id="11:14">PROVIDER DATA MANAGEMENT</div><div className={(styles['class-5']) + " class=WTWERSCOTT"} figma_id="11:15">WTWER SCOTT</div>
+<div className={(styles['class-6']) + " class=Group3028"} figma_id="11:16">
+<div className={(styles['class-7'])}>
+<div className={(styles['class-8']) + " class=Rectangle398955"} figma_id="11:17">
+</div><div className={(styles['class-9']) + " class=NETWORKS"} figma_id="11:18">NETWORKS</div><div className={(styles['class-10']) + " class=CONTRACTS"} figma_id="11:19">CONTRACTS</div>
+<div className={(styles['class-11']) + " class=Rectangle398956"} figma_id="11:20">
+</div><div className={(styles['class-12']) + " class=SEARCH"} figma_id="11:21">SEARCH</div>
+</div>
+</div><div className={(styles['class-13']) + " class=Search"} figma_id="11:22">Search</div>
+<div className={(styles['class-14']) + " class=Rectangle398957"} figma_id="11:23">
+</div>
+<div className={(styles['class-15']) + " class=Rectangle398958"} figma_id="11:24">
+</div>
+<div className={(styles['class-16']) + " class=Rectangle398959"} figma_id="11:25">
+</div><div className={(styles['class-17']) + " class=PROVIDER"} figma_id="11:26">PROVIDER</div><div className={(styles['class-18']) + " class=SEARCH"} figma_id="11:27">SEARCH</div><div className={(styles['class-19']) + " class=AdvancedSearch"} figma_id="11:28">Advanced
+Search</div><div className={(styles['class-20']) + " class=+AddProvider"} figma_id="11:29">+ Add Provider</div><div className={(styles['class-21']) + " class=CORAMRXLLC"} figma_id="11:30">CORAM RX LLC</div>
+<div className={(styles['class-22']) + " class=Polygon3"} figma_id="11:31">
+<svg height="16.000003655803994" width="19.99999949141602" xmlns="http://www.w3.org/2000/svg">
+<polygon fill="rgba(0, 0, 0, 1.0)" points="0,16.000003655803994 9.99999974570801,0 19.99999949141602,16.000003655803994" stroke="none"></polygon>
+</svg>
+</div>
+<div className={(styles['class-23']) + " class=Polygon4"} figma_id="11:32">
+<svg height="16.000003655803994" width="19.99999949141602" xmlns="http://www.w3.org/2000/svg">
+<polygon fill="rgba(0, 0, 0, 1.0)" points="0,16.000003655803994 9.99999974570801,0 19.99999949141602,16.000003655803994" stroke="none"></polygon>
+</svg>
+</div><div className={(styles['class-24']) + " class=NONE"} figma_id="11:33">NONE</div>
+<div className={(styles['class-25']) + " class=Line7"} figma_id="11:34"></div>
+<div className={(styles['class-26']) + " class=Rectangle398961"} figma_id="11:35">
+</div>
+<div className={(styles['class-27']) + " class=Line8"} figma_id="11:36"></div>
+<div className={(styles['class-28']) + " class=SearchResult"} figma_id="11:37">Search Result</div>
+<div className={(styles['class-29']) + " class=Rectangle398962"} figma_id="11:38">
+</div>
+<div className={(styles['class-30']) + " class=Rectangle398963"} figma_id="11:39">
+</div>
+<div className={(styles['class-31']) + " class=Rectangle398964"} figma_id="11:40">
+</div>
+<div className={(styles['class-32']) + " class=Rectangle398965"} figma_id="11:41">
+</div>
+<div className={(styles['class-33']) + " class=Rectangle398966"} figma_id="11:42">
+</div>
+<div className={(styles['class-34']) + " class=Rectangle398967"} figma_id="11:43">
+</div>
+<div className={(styles['class-35']) + " class=Rectangle398968"} figma_id="11:44">
+</div>
+<div className={(styles['class-36']) + " class=Rectangle398969"} figma_id="11:45">
+</div>
+<div className={(styles['class-37']) + " class=Rectangle398970"} figma_id="11:46">
+</div>
+<div className={(styles['class-38']) + " class=Rectangle398971"} figma_id="11:47">
+</div><div className={(styles['class-39']) + " class=PRACTITIONER"} figma_id="11:48">PRACTITIONER</div><div className={(styles['class-40']) + " class=PROVIDER"} figma_id="11:49">PROVIDER</div><div className={(styles['class-41']) + " class=SERVICELOCATION"} figma_id="11:50">SERVICE LOCATION</div><div className={(styles['class-42']) + " class=ADDRESS"} figma_id="11:51">ADDRESS</div><div className={(styles['class-43']) + " class=BUSSINESSENTITY"} figma_id="11:52">BUSSINESS ENTITY</div><div className={(styles['class-44']) + " class=CORPORATEENTITY"} figma_id="11:53">CORPORATE ENTITY</div><div className={(styles['class-45']) + " class=BANKING"} figma_id="11:54">BANKING</div>
+<div className={(styles['class-46']) + " class=Line9"} figma_id="11:55">
+<svg fill="none" height="4" viewbox="0 0 107 4" width="107" xmlns="http://www.w3.org/2000/svg">
+<path d="M2 2H105" stroke="#3EFFC0" stroke-linecap="round" stroke-width="4"></path>
+</svg>
+</div>
+<div className={(styles['class-47']) + " class=TIN"} figma_id="11:56">TIN</div><div className={(styles['class-48']) + " class=4233753"} figma_id="11:57">4233753</div><div className={(styles['class-49']) + " class=4233753"} figma_id="11:58">4233753</div><div className={(styles['class-50']) + " class=4233753"} figma_id="11:59">4233753</div><div className={(styles['class-51']) + " class=4233753"} figma_id="11:60">4233753</div><div className={(styles['class-52']) + " class=4233753"} figma_id="11:61">4233753</div><div className={(styles['class-53']) + " class=4233753"} figma_id="11:62">4233753</div><div className={(styles['class-54']) + " class=4233753"} figma_id="11:63">4233753</div><div className={(styles['class-55']) + " class=4233753"} figma_id="11:64">4233753</div><div className={(styles['class-56']) + " class=4233753"} figma_id="11:65">4233753</div><div className={(styles['class-57']) + " class=NAME"} figma_id="11:66">NAME</div><div className={(styles['class-58']) + " class=TYPE"} figma_id="11:67">TYPE</div><div className={(styles['class-59']) + " class=CORAMRXLLC"} figma_id="11:68">CORAM RX LLC</div><div className={(styles['class-60']) + " class=ABCBUSSINESS"} figma_id="11:69">ABC BUSSINESS</div><div className={(styles['class-61']) + " class=ABCBUSSINESS"} figma_id="11:70">ABC BUSSINESS</div><div className={(styles['class-62']) + " class=ABCBUSSINESS"} figma_id="11:71">ABC BUSSINESS</div><div className={(styles['class-63']) + " class=ABCBUSSINESS"} figma_id="11:72">ABC BUSSINESS</div><div className={(styles['class-64']) + " class=ABCBUSSINESS"} figma_id="11:73">ABC BUSSINESS</div><div className={(styles['class-65']) + " class=ABCBUSSINESS"} figma_id="11:74">ABC BUSSINESS</div><div className={(styles['class-66']) + " class=ABCBUSSINESS"} figma_id="11:75">ABC BUSSINESS</div><div className={(styles['class-67']) + " class=ABCBUSSINESS"} figma_id="11:76">ABC BUSSINESS</div><div className={(styles['class-68']) + " class=ABCBUSSINESS"} figma_id="11:77">ABC BUSSINESS</div><div className={(styles['class-69']) + " class=CORAMRXLLC"} figma_id="11:78">CORAM RX LLC</div><div className={(styles['class-70']) + " class=CORAMRXLLC"} figma_id="11:79">CORAM RX LLC</div><div className={(styles['class-71']) + " class=CORAMRXLLC"} figma_id="11:80">CORAM RX LLC</div><div className={(styles['class-72']) + " class=CORAMRXLLC"} figma_id="11:81">CORAM RX LLC</div><div className={(styles['class-73']) + " class=CORAMRXLLC"} figma_id="11:82">CORAM RX LLC</div><div className={(styles['class-74']) + " class=CORAMRXLLC"} figma_id="11:83">CORAM RX LLC</div><div className={(styles['class-75']) + " class=CORAMRXLLC"} figma_id="11:84">CORAM RX LLC</div><div className={(styles['class-76']) + " class=CORAMRXLLC"} figma_id="11:85">CORAM RX LLC</div>
+</div>
+
+        </>
+    );
+};
+
+export default Design;
