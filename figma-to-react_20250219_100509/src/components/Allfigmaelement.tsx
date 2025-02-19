f4d54d6b513cda49ed9f9c02c243d46e3a20c84b
Aparna-Promptora <babyaparna31@gmail.com>
2025-02-19 10:07:33 +0530

    Add/Update React project in dedicated folder

---
+++ figma-to-react_20250219_100509/src/components/Allfigmaelement.tsx
@@ -0,0 +1,187 @@
+import { React } from 'react';
+import styles from '../styles/Allfigmaelement.module.css';
+import a_09e1103b40f27e796fcfee5dc6e292e23a5a6ec4_jpg from '../assets/a_09e1103b40f27e796fcfee5dc6e292e23a5a6ec4.jpg';
+
+const Allfigmaelement: React.FC = () => {
+    
+    return (
+        <>
+            
+<div className={(styles['class-0']) + " class=Allfigmaelement"} figma_id="33:215">
+<textarea className={(styles['class-1']) + " class=Textarea_textarea"} figma_id="33:216" placeholder="Enter your name"></textarea>
+<input className={(styles['class-2']) + " class=Inputfield_inputfield"} figma_id="33:220" placeholder="Inputfield" type="text"/>
+<button className={(styles['class-3']) + " class=_btn"} figma_id="33:223">
+<span className="text-container">Button</span>
+</button>
+<div className={(styles['class-4'])}>
+<select className={(styles['class-5']) + " class=Dropdown_dropdown"} figma_id="33:226">
+<option selected="" value="">Text</option>
+<option value="option1">Option 1</option>
+<option value="option2">Option 2</option>
+<option value="option3">Option 3</option>
+</select>
+<div className={(styles['class-6'])}>
+<svg height="19.599273681640625" width="21.5960693359375" xmlns="http://www.w3.org/2000/svg">
+<polygon fill="rgba(255, 255, 255, 1.0)" points="0,19.599273681640625 10.79803466796875,0 21.5960693359375,19.599273681640625" stroke="none"></polygon>
+</svg>
+</div>
+</div>
+<input checked="" className={(styles['class-7'])} type="checkbox"/>
+<input checked="checked" className={(styles['class-8'])} type="radio"/>
+<div className={(styles['class-9']) + " class=card"} figma_id="33:238">
+<div className={(styles['class-10'])}>
+<div className={(styles['class-11']) + " class=Cardbg"} figma_id="33:239">
+</div><div className={(styles['class-12']) + " class=CardTitle"} figma_id="33:240">Card Title</div><div className={(styles['class-13']) + " class=Itisalongestablishedfactthatareaderwillbedistractedbythereadablecontentofapagewhenlookingatitslayout.ThepointofusingLoremIpsumisthatithasamore-or-lessnormaldistributionofletters,asopposedtousing'Contenthere,contenthere',makingitlooklikereadableEnglish."} figma_id="33:241">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</div>
+<img className={(styles['class-14']) + " class=Image"} figma_id="33:242" src={a_09e1103b40f27e796fcfee5dc6e292e23a5a6ec4_jpg}/>
+</div>
+</div>
+<div className={(styles['class-15']) + " class=popup"} figma_id="33:243">
+<div className={(styles['class-16'])}>
+<div className={(styles['class-17']) + " class=Rectangle475"} figma_id="33:244">
+</div><div className={(styles['class-18']) + " class=Acceptchanges?"} figma_id="33:245">Accept changes?</div><div className={(styles['class-19']) + " class=Paragraph"} figma_id="33:246">Malesuada tellus tincidunt fringilla enim, id mauris. Id etiam nibh suscipit aliquam dolor.</div>
+<div className={(styles['class-20']) + " class=Rectangle476"} figma_id="33:247">
+</div><div className={(styles['class-21']) + " class=🤨"} figma_id="33:248">🤨</div>
+<button className={(styles['class-22']) + " class=Reject_btn"} figma_id="33:249">
+<span className="text-container">Reject</span>
+</button>
+<button className={(styles['class-23']) + " class=Accept_btn"} figma_id="33:252">
+<span className="text-container">Accept</span>
+</button>
+<div className={(styles['class-24']) + " class=Crossicon"} figma_id="33:255">
+<div className={(styles['class-25'])}>
+<div className={(styles['class-26']) + " class=Ellipse3"} figma_id="33:256"></div>
+<div className={(styles['class-27']) + " class=Vector"} figma_id="33:257">
+<svg fill="none" height="10" viewbox="0 0 10 10" width="10" xmlns="http://www.w3.org/2000/svg">
+<path d="M5 3.88906L8.88906 0L10 1.11094L6.11094 5L10 8.88906L8.88906 10L5 6.11094L1.11094 10L0 8.88906L3.88906 5L0 1.11094L1.11094 0L5 3.88906Z" fill="black"></path>
+</svg>
+</div>
+</div>
+</div>
+</div>
+</div>
+<details className={(styles['class-28']) + " class=Accordian_accordian"} figma_id="33:258">
+<summary className={(styles['class-29'])}>
+<span className={(styles['class-30'])}>
+                                Is this a question?
+                            </span>
+<span className={(styles['class-31']) + " class=accordion-svg-container"}>
+<span className={(styles['class-32'])}>
+<svg fill="none" height="27" viewbox="0 0 27 27" width="27" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 3.99056C0 1.78663 1.78663 0 3.99056 0H17.5754C19.7794 0 21.566 1.78663 21.566 3.99056V5.43395H23.0094C25.2133 5.43395 26.9999 7.22058 26.9999 9.42451V17.5754C26.9999 19.7794 25.2133 21.566 23.0094 21.566H21.566V25.7264C21.566 26.2415 21.2557 26.7059 20.7798 26.903C20.3039 27.1001 19.7561 26.9912 19.3918 26.6269L14.3309 21.566H9.42451C8.78763 21.566 8.18491 21.4162 7.65046 21.1506L7.60809 21.193C7.24385 21.5572 6.69606 21.6662 6.22015 21.469C5.74425 21.2719 5.43395 20.8075 5.43395 20.2924V16.132H3.99056C1.78663 16.132 0 14.3454 0 12.1415V3.99056ZM9.78223 19.0188H14.8585C15.1962 19.0188 15.5202 19.153 15.759 19.3918L19.0188 22.6517V20.2924C19.0188 19.589 19.589 19.0188 20.2924 19.0188H23.0094C23.8065 19.0188 24.4528 18.3726 24.4528 17.5754V9.42451C24.4528 8.62734 23.8065 7.98111 23.0094 7.98111H21.566V12.1415C21.566 14.3454 19.7794 16.132 17.5754 16.132H12.669L9.78223 19.0188ZM19.0188 3.99056C19.0188 3.19339 18.3726 2.54716 17.5754 2.54716H3.99056C3.19339 2.54716 2.54716 3.19339 2.54716 3.99056V12.1415C2.54716 12.9386 3.19339 13.5849 3.99056 13.5849H6.70753C7.41091 13.5849 7.98111 14.1551 7.98111 14.8585V17.2177L11.2409 13.9579C11.4798 13.7191 11.8037 13.5849 12.1415 13.5849H17.5754C18.3726 13.5849 19.0188 12.9386 19.0188 12.1415V3.99056Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-33'])}>
+<svg fill="none" height="27" viewbox="0 0 27 27" width="27" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 3.99056C0 1.78663 1.78663 0 3.99056 0H17.5754C19.7794 0 21.566 1.78663 21.566 3.99056V5.43395H23.0094C25.2133 5.43395 26.9999 7.22058 26.9999 9.42451V17.5754C26.9999 19.7794 25.2133 21.566 23.0094 21.566H21.566V25.7264C21.566 26.2415 21.2557 26.7059 20.7798 26.903C20.3039 27.1001 19.7561 26.9912 19.3918 26.6269L14.3309 21.566H9.42451C8.78763 21.566 8.18491 21.4162 7.65046 21.1506L7.60809 21.193C7.24385 21.5572 6.69606 21.6662 6.22015 21.469C5.74425 21.2719 5.43395 20.8075 5.43395 20.2924V16.132H3.99056C1.78663 16.132 0 14.3454 0 12.1415V3.99056ZM9.78223 19.0188H14.8585C15.1962 19.0188 15.5202 19.153 15.759 19.3918L19.0188 22.6517V20.2924C19.0188 19.589 19.589 19.0188 20.2924 19.0188H23.0094C23.8065 19.0188 24.4528 18.3726 24.4528 17.5754V9.42451C24.4528 8.62734 23.8065 7.98111 23.0094 7.98111H21.566V12.1415C21.566 14.3454 19.7794 16.132 17.5754 16.132H12.669L9.78223 19.0188ZM19.0188 3.99056C19.0188 3.19339 18.3726 2.54716 17.5754 2.54716H3.99056C3.19339 2.54716 2.54716 3.19339 2.54716 3.99056V12.1415C2.54716 12.9386 3.19339 13.5849 3.99056 13.5849H6.70753C7.41091 13.5849 7.98111 14.1551 7.98111 14.8585V17.2177L11.2409 13.9579C11.4798 13.7191 11.8037 13.5849 12.1415 13.5849H17.5754C18.3726 13.5849 19.0188 12.9386 19.0188 12.1415V3.99056Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-34'])}>
+<svg fill="none" height="27" viewbox="0 0 27 27" width="27" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 3.99056C0 1.78663 1.78663 0 3.99056 0H17.5754C19.7794 0 21.566 1.78663 21.566 3.99056V5.43395H23.0094C25.2133 5.43395 26.9999 7.22058 26.9999 9.42451V17.5754C26.9999 19.7794 25.2133 21.566 23.0094 21.566H21.566V25.7264C21.566 26.2415 21.2557 26.7059 20.7798 26.903C20.3039 27.1001 19.7561 26.9912 19.3918 26.6269L14.3309 21.566H9.42451C8.78763 21.566 8.18491 21.4162 7.65046 21.1506L7.60809 21.193C7.24385 21.5572 6.69606 21.6662 6.22015 21.469C5.74425 21.2719 5.43395 20.8075 5.43395 20.2924V16.132H3.99056C1.78663 16.132 0 14.3454 0 12.1415V3.99056ZM9.78223 19.0188H14.8585C15.1962 19.0188 15.5202 19.153 15.759 19.3918L19.0188 22.6517V20.2924C19.0188 19.589 19.589 19.0188 20.2924 19.0188H23.0094C23.8065 19.0188 24.4528 18.3726 24.4528 17.5754V9.42451C24.4528 8.62734 23.8065 7.98111 23.0094 7.98111H21.566V12.1415C21.566 14.3454 19.7794 16.132 17.5754 16.132H12.669L9.78223 19.0188ZM19.0188 3.99056C19.0188 3.19339 18.3726 2.54716 17.5754 2.54716H3.99056C3.19339 2.54716 2.54716 3.19339 2.54716 3.99056V12.1415C2.54716 12.9386 3.19339 13.5849 3.99056 13.5849H6.70753C7.41091 13.5849 7.98111 14.1551 7.98111 14.8585V17.2177L11.2409 13.9579C11.4798 13.7191 11.8037 13.5849 12.1415 13.5849H17.5754C18.3726 13.5849 19.0188 12.9386 19.0188 12.1415V3.99056Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-35'])}>
+<svg fill="none" height="16" viewbox="0 0 10 16" width="10" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.46863 15.5314C-0.156209 14.9065 -0.156209 13.8935 0.46863 13.2686L5.73726 8L0.468629 2.73137C-0.15621 2.10653 -0.15621 1.09347 0.468629 0.46863C1.09347 -0.15621 2.10653 -0.15621 2.73137 0.46863L9.13137 6.86863C9.75621 7.49347 9.75621 8.50653 9.13137 9.13137L2.73137 15.5314C2.10653 16.1562 1.09347 16.1562 0.46863 15.5314Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-36'])}>
+<svg fill="none" height="16" viewbox="0 0 10 16" width="10" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.46863 15.5314C-0.156209 14.9065 -0.156209 13.8935 0.46863 13.2686L5.73726 8L0.468629 2.73137C-0.15621 2.10653 -0.15621 1.09347 0.468629 0.46863C1.09347 -0.15621 2.10653 -0.15621 2.73137 0.46863L9.13137 6.86863C9.75621 7.49347 9.75621 8.50653 9.13137 9.13137L2.73137 15.5314C2.10653 16.1562 1.09347 16.1562 0.46863 15.5314Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-37'])}>
+<svg fill="none" height="10" viewbox="0 0 16 10" width="16" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.46863 0.468629C1.09347 -0.15621 2.10653 -0.15621 2.73137 0.468629L8 5.73726L13.2686 0.468629C13.8935 -0.15621 14.9065 -0.15621 15.5314 0.468629C16.1562 1.09347 16.1562 2.10653 15.5314 2.73137L9.13137 9.13137C8.50653 9.75621 7.49347 9.75621 6.86863 9.13137L0.46863 2.73137C-0.15621 2.10653 -0.15621 1.09347 0.46863 0.468629Z" fill="#13151A"></path>
+</svg>
+</span>
+</span>
+</summary>
+<p className={(styles['class-38'])}>Answer: Neque, lacus, libero id urna, sollicitudin in phasellus pellentesque ultricies.</p>
+<summary className={(styles['class-39'])}>
+<span className={(styles['class-40'])}>
+                                Is this a question?
+                            </span>
+<span className={(styles['class-41']) + " class=accordion-svg-container"}>
+<span className={(styles['class-42'])}>
+<svg fill="none" height="27" viewbox="0 0 27 27" width="27" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 3.99056C0 1.78663 1.78663 0 3.99056 0H17.5754C19.7794 0 21.566 1.78663 21.566 3.99056V5.43395H23.0094C25.2133 5.43395 26.9999 7.22058 26.9999 9.42451V17.5754C26.9999 19.7794 25.2133 21.566 23.0094 21.566H21.566V25.7264C21.566 26.2415 21.2557 26.7059 20.7798 26.903C20.3039 27.1001 19.7561 26.9912 19.3918 26.6269L14.3309 21.566H9.42451C8.78763 21.566 8.18491 21.4162 7.65046 21.1506L7.60809 21.193C7.24385 21.5572 6.69606 21.6662 6.22015 21.469C5.74425 21.2719 5.43395 20.8075 5.43395 20.2924V16.132H3.99056C1.78663 16.132 0 14.3454 0 12.1415V3.99056ZM9.78223 19.0188H14.8585C15.1962 19.0188 15.5202 19.153 15.759 19.3918L19.0188 22.6517V20.2924C19.0188 19.589 19.589 19.0188 20.2924 19.0188H23.0094C23.8065 19.0188 24.4528 18.3726 24.4528 17.5754V9.42451C24.4528 8.62734 23.8065 7.98111 23.0094 7.98111H21.566V12.1415C21.566 14.3454 19.7794 16.132 17.5754 16.132H12.669L9.78223 19.0188ZM19.0188 3.99056C19.0188 3.19339 18.3726 2.54716 17.5754 2.54716H3.99056C3.19339 2.54716 2.54716 3.19339 2.54716 3.99056V12.1415C2.54716 12.9386 3.19339 13.5849 3.99056 13.5849H6.70753C7.41091 13.5849 7.98111 14.1551 7.98111 14.8585V17.2177L11.2409 13.9579C11.4798 13.7191 11.8037 13.5849 12.1415 13.5849H17.5754C18.3726 13.5849 19.0188 12.9386 19.0188 12.1415V3.99056Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-43'])}>
+<svg fill="none" height="27" viewbox="0 0 27 27" width="27" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 3.99056C0 1.78663 1.78663 0 3.99056 0H17.5754C19.7794 0 21.566 1.78663 21.566 3.99056V5.43395H23.0094C25.2133 5.43395 26.9999 7.22058 26.9999 9.42451V17.5754C26.9999 19.7794 25.2133 21.566 23.0094 21.566H21.566V25.7264C21.566 26.2415 21.2557 26.7059 20.7798 26.903C20.3039 27.1001 19.7561 26.9912 19.3918 26.6269L14.3309 21.566H9.42451C8.78763 21.566 8.18491 21.4162 7.65046 21.1506L7.60809 21.193C7.24385 21.5572 6.69606 21.6662 6.22015 21.469C5.74425 21.2719 5.43395 20.8075 5.43395 20.2924V16.132H3.99056C1.78663 16.132 0 14.3454 0 12.1415V3.99056ZM9.78223 19.0188H14.8585C15.1962 19.0188 15.5202 19.153 15.759 19.3918L19.0188 22.6517V20.2924C19.0188 19.589 19.589 19.0188 20.2924 19.0188H23.0094C23.8065 19.0188 24.4528 18.3726 24.4528 17.5754V9.42451C24.4528 8.62734 23.8065 7.98111 23.0094 7.98111H21.566V12.1415C21.566 14.3454 19.7794 16.132 17.5754 16.132H12.669L9.78223 19.0188ZM19.0188 3.99056C19.0188 3.19339 18.3726 2.54716 17.5754 2.54716H3.99056C3.19339 2.54716 2.54716 3.19339 2.54716 3.99056V12.1415C2.54716 12.9386 3.19339 13.5849 3.99056 13.5849H6.70753C7.41091 13.5849 7.98111 14.1551 7.98111 14.8585V17.2177L11.2409 13.9579C11.4798 13.7191 11.8037 13.5849 12.1415 13.5849H17.5754C18.3726 13.5849 19.0188 12.9386 19.0188 12.1415V3.99056Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-44'])}>
+<svg fill="none" height="27" viewbox="0 0 27 27" width="27" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 3.99056C0 1.78663 1.78663 0 3.99056 0H17.5754C19.7794 0 21.566 1.78663 21.566 3.99056V5.43395H23.0094C25.2133 5.43395 26.9999 7.22058 26.9999 9.42451V17.5754C26.9999 19.7794 25.2133 21.566 23.0094 21.566H21.566V25.7264C21.566 26.2415 21.2557 26.7059 20.7798 26.903C20.3039 27.1001 19.7561 26.9912 19.3918 26.6269L14.3309 21.566H9.42451C8.78763 21.566 8.18491 21.4162 7.65046 21.1506L7.60809 21.193C7.24385 21.5572 6.69606 21.6662 6.22015 21.469C5.74425 21.2719 5.43395 20.8075 5.43395 20.2924V16.132H3.99056C1.78663 16.132 0 14.3454 0 12.1415V3.99056ZM9.78223 19.0188H14.8585C15.1962 19.0188 15.5202 19.153 15.759 19.3918L19.0188 22.6517V20.2924C19.0188 19.589 19.589 19.0188 20.2924 19.0188H23.0094C23.8065 19.0188 24.4528 18.3726 24.4528 17.5754V9.42451C24.4528 8.62734 23.8065 7.98111 23.0094 7.98111H21.566V12.1415C21.566 14.3454 19.7794 16.132 17.5754 16.132H12.669L9.78223 19.0188ZM19.0188 3.99056C19.0188 3.19339 18.3726 2.54716 17.5754 2.54716H3.99056C3.19339 2.54716 2.54716 3.19339 2.54716 3.99056V12.1415C2.54716 12.9386 3.19339 13.5849 3.99056 13.5849H6.70753C7.41091 13.5849 7.98111 14.1551 7.98111 14.8585V17.2177L11.2409 13.9579C11.4798 13.7191 11.8037 13.5849 12.1415 13.5849H17.5754C18.3726 13.5849 19.0188 12.9386 19.0188 12.1415V3.99056Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-45'])}>
+<svg fill="none" height="16" viewbox="0 0 10 16" width="10" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.46863 15.5314C-0.156209 14.9065 -0.156209 13.8935 0.46863 13.2686L5.73726 8L0.468629 2.73137C-0.15621 2.10653 -0.15621 1.09347 0.468629 0.46863C1.09347 -0.15621 2.10653 -0.15621 2.73137 0.46863L9.13137 6.86863C9.75621 7.49347 9.75621 8.50653 9.13137 9.13137L2.73137 15.5314C2.10653 16.1562 1.09347 16.1562 0.46863 15.5314Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-46'])}>
+<svg fill="none" height="16" viewbox="0 0 10 16" width="10" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.46863 15.5314C-0.156209 14.9065 -0.156209 13.8935 0.46863 13.2686L5.73726 8L0.468629 2.73137C-0.15621 2.10653 -0.15621 1.09347 0.468629 0.46863C1.09347 -0.15621 2.10653 -0.15621 2.73137 0.46863L9.13137 6.86863C9.75621 7.49347 9.75621 8.50653 9.13137 9.13137L2.73137 15.5314C2.10653 16.1562 1.09347 16.1562 0.46863 15.5314Z" fill="#13151A"></path>
+</svg>
+</span>
+<span className={(styles['class-47'])}>
+<svg fill="none" height="10" viewbox="0 0 16 10" width="16" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.46863 0.468629C1.09347 -0.15621 2.10653 -0.15621 2.73137 0.468629L8 5.73726L13.2686 0.468629C13.8935 -0.15621 14.9065 -0.15621 15.5314 0.468629C16.1562 1.09347 16.1562 2.10653 15.5314 2.73137L9.13137 9.13137C8.50653 9.75621 7.49347 9.75621 6.86863 9.13137L0.46863 2.73137C-0.15621 2.10653 -0.15621 1.09347 0.46863 0.468629Z" fill="#13151A"></path>
+</svg>
+</span>
+</span>
+</summary>
+<p className={(styles['class-48'])}>Is this a question?</p>
+</details>
+<div className={(styles['class-49']) + " class=Tab_tabs"} figma_id="33:274" role="tablist">
+<button aria-controls="panel-0" aria-selected="true" className={(styles['class-50']) + " class=tab-button"} id="tab-0" role="tab">
+<span className={(styles['class-51'])}>
+                        Dashboard
+                    </span>
+<span className={(styles['class-52']) + " class=active-indicator"}></span>
+</button>
+<button aria-controls="panel-1" aria-selected="false" className={(styles['class-53']) + " class=tab-button"} id="tab-1" role="tab">
+<span className={(styles['class-54'])}>
+                        Dashboard
+                    </span>
+</button>
+<button aria-controls="panel-2" aria-selected="false" className={(styles['class-55']) + " class=tab-button"} id="tab-2" role="tab">
+<span className={(styles['class-56'])}>
+                        Dashboard
+                    </span>
+</button></div>
+<div className={(styles['class-57']) + " class=Progressbar"} figma_id="33:282">
+<div className={(styles['class-58'])}>
+<div className={(styles['class-59']) + " class=Rectangle480"} figma_id="33:283">
+</div>
+<div className={(styles['class-60']) + " class=Rectangle481"} figma_id="33:284">
+</div>
+</div>
+</div>
+<div className={(styles['class-61']) + " class=Tooltip_tooltip"} figma_id="33:285">
+<div className={(styles['class-62'])}>
+<div className={(styles['class-63']) + " class=Rectangle478"} figma_id="33:286">
+</div>
+<div className={(styles['class-64']) + " class=Polygon2"} figma_id="33:287">
+<svg height="17.36471114307642" width="34.18088125064969" xmlns="http://www.w3.org/2000/svg">
+<polygon fill="rgba(9, 132, 233, 1.0)" points="0,17.36471114307642 17.090440625324845,0 34.18088125064969,17.36471114307642" stroke="none"></polygon>
+</svg>
+</div><div className={(styles['class-65']) + " class=ToolTip"} figma_id="33:288">Tool Tip</div>
+</div>
+</div>
+<div className={(styles['class-66']) + " class=Spinner"} figma_id="33:289">
+<div className={(styles['class-67'])}>
+<div className={(styles['class-68']) + " class=Ellipse1"} figma_id="33:290"></div>
+</div>
+</div>
+</div>
+
+        </>
+    );
+};
+
+export default Allfigmaelement;
