0e21f42d2b00c10a070253aa9e0ceaad1d1c4c2e
Aparna-Promptora <babyaparna31@gmail.com>
2025-02-21 17:15:51 +0530

    Add/Update React project in dedicated folder

---
+++ Figma_to_Angular_20250221171409/src/app/components/Allfigmaelement/Allfigmaelement.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-Allfigmaelement',
+    templateUrl: './Allfigmaelement.component.html',
+    styleUrls: ['./Allfigmaelement.component.scss']
+})
+export class AllfigmaelementComponent {}
---
+++ Figma_to_Angular_20250221171409/src/app/components/Hr/hr.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-hr',
+    templateUrl: './hr.component.html',
+    styleUrls: ['./hr.component.scss']
+})
+export class HrComponent {}
