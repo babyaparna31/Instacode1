6f150755cba9caf6c222b3bccb2c3ca255065c08
Aparna-Promptora <babyaparna31@gmail.com>
2025-02-25 14:27:07 +0530

    Add/Update React project in dedicated folder

---
+++ figma-to-react_20250225_142557/src/App.tsx
@@ -0,0 +1,17 @@
+
+                import React from 'react';
+                import Home from './components/Home';
+import Hr from './components/Hr';
+                import './App.css';
+
+                const App: React.FC = () => {
+                    return (
+                        <div className="App">
+                            <Home />
+<Hr />
+                        </div>
+                    );
+                };
+
+                export default App;
+                
