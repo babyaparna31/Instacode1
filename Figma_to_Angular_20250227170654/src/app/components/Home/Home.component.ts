22b4b948005e1e672ed8e2a4fcb5a6ac7baff6d3
babyaparna31 <babyaparna31@gmail.com>
2025-02-27 17:10:12 +0000

    Add/Update React project in dedicated folder

---
+++ Figma_to_Angular_20250227170654/src/app/components/Home/Home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+
+    @Component({
+        selector: 'app-Home',
+        templateUrl: './Home.component.html',
+        styleUrls: ['./Home.component.scss'],
+        imports:[CommonModule]
+    })
+    export class HomeComponent {}
+    
