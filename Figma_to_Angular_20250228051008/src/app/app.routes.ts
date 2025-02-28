11d2723131c6d28c3e627f0846d8c918d6abd3c9
babyaparna31 <babyaparna31@gmail.com>
2025-02-28 05:11:37 +0000

    Add/Update React project in dedicated folder

---
+++ Figma_to_Angular_20250228051008/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+            import { HomeComponent } from './components/Home/Home.component';
+import { HrComponent } from './components/Hr/Hr.component';
+
+            export const routes: Routes = [
+              { path: 'Home', component: HomeComponent },
+  { path: 'Hr', component: HrComponent }
+            ];
+            
---
+++ Figma_to_Angular_20250228051008/src/app/components/Hr/Hr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+
+    @Component({
+        selector: 'app-Hr',
+        templateUrl: './Hr.component.html',
+        styleUrls: ['./Hr.component.scss'],
+        imports:[CommonModule]
+    })
+    export class HrComponent {}
+    
