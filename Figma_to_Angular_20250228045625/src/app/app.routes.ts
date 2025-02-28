a5f3496cec28e53de2c05754a6b13f828cff06d2
babyaparna31 <babyaparna31@gmail.com>
2025-02-28 10:44:00 +0530

    Delete Figma_to_Angular_20250228045625 directory

--- Figma_to_Angular_20250228045625/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-            import { HomeComponent } from './components/Home/Home.component';
-import { HrComponent } from './components/Hr/Hr.component';
-
-            export const routes: Routes = [
-              { path: 'Home', component: HomeComponent },
-  { path: 'Hr', component: HrComponent }
-            ];
-            
