b5df30b7eee96dc48127d781a3c05b0cf6d36e3a
babyaparna31 <babyaparna31@gmail.com>
2025-02-28 04:58:01 +0000

    Add/Update React project in dedicated folder

---
+++ Figma_to_Angular_20250228045625/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { RouterOutlet} from '@angular/router';
+            import {Component } from '@angular/core';
+            import { BrowserModule } from '@angular/platform-browser';
+            import { HomeComponent } from './components/Home/Home.component';
+import { HrComponent } from './components/Hr/Hr.component';
+
+            @Component({
+            selector: 'app-root',
+            standalone: true,
+            imports: [RouterOutlet, HomeComponent, HrComponent],
+            template: '<router-outlet></router-outlet>'
+            })
+
+            export class AppComponent {
+            title = 'Figma_to_Angular_20250228045625';
+            }
+            
