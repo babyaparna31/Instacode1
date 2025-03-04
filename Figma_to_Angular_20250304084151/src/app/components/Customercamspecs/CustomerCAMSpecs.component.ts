182922b4088c4e598e04617d1a81c05ea2b20b7f
babyaparna31 <babyaparna31@gmail.com>
2025-03-04 08:43:24 +0000

    Add/Update React project in dedicated folder

---
+++ Figma_to_Angular_20250304084151/src/app/components/Customercamspecs/CustomerCAMSpecs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+
+    @Component({
+        selector: 'app-CustomerCAMSpecs',
+        templateUrl: './CustomerCAMSpecs.component.html',
+        styleUrls: ['./CustomerCAMSpecs.component.scss'],
+        imports:[CommonModule]
+    })
+    export class CustomercamspecsComponent {}
+    
---
+++ Figma_to_Angular_20250304084151/src/app/components/Loginpage/Loginpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+
+    @Component({
+        selector: 'app-Loginpage',
+        templateUrl: './Loginpage.component.html',
+        styleUrls: ['./Loginpage.component.scss'],
+        imports:[CommonModule]
+    })
+    export class LoginpageComponent {}
+    
