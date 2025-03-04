841c25d691f21b7d5df805c7c5778d92ba5ed617
babyaparna31 <babyaparna31@gmail.com>
2025-03-04 19:52:36 +0530

    Delete Figma_to_Angular_20250303170357 directory

--- Figma_to_Angular_20250303170357/src/app/components/Customercamspecs/CustomerCAMSpecs.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-    @Component({
-        selector: 'app-CustomerCAMSpecs',
-        templateUrl: './CustomerCAMSpecs.component.html',
-        styleUrls: ['./CustomerCAMSpecs.component.scss']
-    })
-    export class CustomercamspecsComponent {}
-    
