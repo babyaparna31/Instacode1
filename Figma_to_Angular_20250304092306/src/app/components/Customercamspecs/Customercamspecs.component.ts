a7cf46996c86afa892fbc661b3af530eb8acbd2c
babyaparna31 <babyaparna31@gmail.com>
2025-03-04 09:24:43 +0000

    Add/Update React project in dedicated folder

---
+++ Figma_to_Angular_20250304092306/src/app/components/Customercamspecs/Customercamspecs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+    import { CommonModule } from '@angular/common';
+
+    @Component({
+        selector: 'app-Customercamspecs',
+        templateUrl: './Customercamspecs.component.html',
+        styleUrls: ['./Customercamspecs.component.scss'],
+        imports:[CommonModule]
+    })
+    export class CustomercamspecsComponent {}
+    
