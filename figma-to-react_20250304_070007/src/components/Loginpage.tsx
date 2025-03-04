274f72926df804f26b9111589faaf56e49a550d2
babyaparna31 <babyaparna31@gmail.com>
2025-03-04 07:01:29 +0000

    Add/Update React project in dedicated folder

---
+++ figma-to-react_20250304_070007/src/components/Loginpage.tsx
@@ -0,0 +1,81 @@
+import { React } from 'react';
+import styles from '../styles/Loginpage.module.css';
+import a_f01a654c71c4e54e396fb7bbd74a102854be71df_jpg from '../assets/a_f01a654c71c4e54e396fb7bbd74a102854be71df.jpg';
+import a_483874c40a6e2a7169ce83fd923590f7548a4fe1_jpg from '../assets/a_483874c40a6e2a7169ce83fd923590f7548a4fe1.jpg';
+
+const Loginpage: React.FC = () => {
+    
+    return (
+        <>
+            
+<div className={(styles['class-0']) + " class=Loginpage"} figma_id="1:5">
+<div className={(styles['class-1']) + " class=Group6266"} figma_id="1:38">
+<div className={(styles['class-2'])}>
+<div className={(styles['class-3']) + " class=MichelinRetreadTechnologiesBibTreadNext"} figma_id="1:7">Michelin Retread Technologies
+Bib Tread Next</div>
+</div>
+</div><div className={(styles['class-4']) + " class=Dealer*"} figma_id="1:8">Dealer*</div><div className={(styles['class-5']) + " class=UserID*"} figma_id="1:9">User ID*</div><div className={(styles['class-6']) + " class=Password*"} figma_id="1:10">Password*</div>
+<div className={(styles['class-7']) + " class=dropdown-container"}>
+<select className={(styles['class-8']) + " class=Dropdown"} figma_id="1:11">
+<option selected="" value="">MILLER</option>
+<option value="option1">Option 1</option>
+<option value="option2">Option 2</option>
+<option value="option3">Option 3</option>
+</select>
+<div className={(styles['class-9']) + " class=Line11"} figma_id="1:13"></div>
+</div>
+<div className={(styles['class-10'])}>
+<input className={(styles['class-11']) + " class=Inputfield"} figma_id="1:15" placeholder="Please enter User ID here" type="text"/>
+<div className={(styles['class-12']) + " class=Line11"} figma_id="1:17"></div>
+</div>
+<div className={(styles['class-13'])}>
+<input className={(styles['class-14']) + " class=Inputfield"} figma_id="1:18" placeholder="Please enter User ID here" type="text"/>
+<div className={(styles['class-15']) + " class=Line11"} figma_id="1:20"></div>
+<div className={(styles['class-16']) + " class=vector-container"}>
+<div className={(styles['class-17'])}>
+<svg fill="none" height="13" viewbox="0 0 15 13" width="15" xmlns="http://www.w3.org/2000/svg">
+<path clip-rule="evenodd" d="M3.99533 2.50334C6.75902 2.50334 9.00201 4.74633 9.00201 7.51002C9.00201 8.16089 8.87184 8.77171 8.64153 9.34247L11.5654 12.2664C13.0775 11.0047 14.269 9.37251 15 7.51002C13.2677 3.11416 8.992 0 3.98532 0C2.58345 0 1.24166 0.250334 0 0.700935L2.16289 2.86382C2.73365 2.63351 3.34446 2.50334 3.99533 2.50334Z" fill="black" fill-rule="evenodd"></path>
+</svg>
+</div>
+<div className={(styles['class-18'])}>
+<svg fill="none" height="19" viewbox="0 0 20 19" width="20" xmlns="http://www.w3.org/2000/svg">
+<path clip-rule="evenodd" d="M1 1.27L3.28 3.55L3.74 4.01C2.08 5.3 0.78 7.02 0 9C1.73 13.39 6 16.5 11 16.5C12.55 16.5 14.03 16.2 15.38 15.66L15.8 16.08L18.73 19L20 17.73L2.27 0L1 1.27Z" fill="black" fill-rule="evenodd"></path>
+</svg>
+</div>
+<div className={(styles['class-19'])}>
+<svg fill="none" height="8" viewbox="0 0 8 8" width="8" xmlns="http://www.w3.org/2000/svg">
+<path clip-rule="evenodd" d="M0.588888 0L2.31111 1.72222C2.25555 1.95555 2.22222 2.2 2.22222 2.44444C2.22222 4.28888 3.7111 5.77777 5.55555 5.77777C5.79999 5.77777 6.04443 5.74443 6.27777 5.68888L7.99999 7.4111C7.25554 7.77776 6.43332 7.99999 5.55555 7.99999C2.48888 7.99999 0 5.5111 0 2.44444C0 1.56666 0.222222 0.744443 0.588888 0Z" fill="white" fill-rule="evenodd"></path>
+</svg>
+</div>
+<div className={(styles['class-20'])}>
+<svg fill="none" height="4" viewbox="0 0 4 4" width="4" xmlns="http://www.w3.org/2000/svg">
+<path clip-rule="evenodd" d="M0 0.0126184L3.97478 3.9874L4.00002 3.78551C4.00002 1.69086 2.30916 0 0.214512 0L0 0.0126184Z" fill="black" fill-rule="evenodd"></path>
+</svg>
+</div>
+</div>
+</div>
+<div className={(styles['class-21']) + " class=Group6265"} figma_id="1:26">
+<div className={(styles['class-22'])}>
+<div className={(styles['class-23']) + " class=Rectangle129"} figma_id="1:27">
+</div>
+<div className={(styles['class-24']) + " class=Vector"} figma_id="1:28">
+<svg fill="none" height="16" viewbox="0 0 16 16" width="16" xmlns="http://www.w3.org/2000/svg">
+<path d="M8 11.9838C8.19272 11.9838 8.35425 11.9186 8.48461 11.7883C8.61497 11.6579 8.68015 11.4964 8.68015 11.3036C8.68015 11.1109 8.61497 10.9494 8.48461 10.819C8.35425 10.6887 8.19272 10.6235 8 10.6235C7.80728 10.6235 7.64575 10.6887 7.51539 10.819C7.38503 10.9494 7.31985 11.1109 7.31985 11.3036C7.31985 11.4964 7.38503 11.6579 7.51539 11.7883C7.64575 11.9186 7.80728 11.9838 8 11.9838ZM8.00027 8.90689C8.17931 8.90689 8.32928 8.84636 8.45019 8.72531C8.5711 8.60427 8.63156 8.45428 8.63156 8.27533V4.4858C8.63156 4.30687 8.57101 4.15688 8.44992 4.03582C8.32881 3.91477 8.17874 3.85424 7.99973 3.85424C7.82069 3.85424 7.67072 3.91477 7.54981 4.03582C7.4289 4.15688 7.36844 4.30687 7.36844 4.4858V8.27533C7.36844 8.45428 7.42899 8.60427 7.55008 8.72531C7.67119 8.84636 7.82125 8.90689 8.00027 8.90689ZM8.00141 16C6.89494 16 5.85491 15.79 4.88132 15.3701C3.90772 14.9502 3.06082 14.3803 2.34064 13.6604C1.62044 12.9405 1.05028 12.094 0.63017 11.1208C0.210057 10.1477 0 9.10788 0 8.00141C0 6.89494 0.209966 5.85491 0.629896 4.88132C1.04983 3.90772 1.61972 3.06082 2.33958 2.34064C3.05946 1.62044 3.90598 1.05028 4.87915 0.630171C5.8523 0.210058 6.89212 0 7.99859 0C9.10506 0 10.1451 0.209966 11.1187 0.629897C12.0923 1.04983 12.9392 1.61972 13.6594 2.33959C14.3796 3.05946 14.9497 3.90598 15.3698 4.87915C15.7899 5.8523 16 6.89212 16 7.99859C16 9.10506 15.79 10.1451 15.3701 11.1187C14.9502 12.0923 14.3803 12.9392 13.6604 13.6594C12.9405 14.3796 12.094 14.9497 11.1208 15.3698C10.1477 15.7899 9.10788 16 8.00141 16ZM8 14.7369C9.88071 14.7369 11.4737 14.0842 12.779 12.779C14.0842 11.4737 14.7369 9.88071 14.7369 8C14.7369 6.11929 14.0842 4.52631 12.779 3.22104C11.4737 1.91577 9.88071 1.26314 8 1.26314C6.11929 1.26314 4.52631 1.91577 3.22104 3.22104C1.91577 4.52631 1.26314 6.11929 1.26314 8C1.26314 9.88071 1.91577 11.4737 3.22104 12.779C4.52631 14.0842 6.11929 14.7369 8 14.7369Z" fill="#0D0D0D"></path>
+</svg>
+</div>
+<div className={(styles['class-25']) + " class=✏️Message"} figma_id="1:29">You only have 5 login attempts left before your account is temporarily blocked</div>
+</div>
+</div>
+<button className={(styles['class-26']) + " class=Button"} figma_id="1:30">
+                Login
+                
+            </button><div className={(styles['class-27']) + " class=Poweredby"} figma_id="1:35">Powered by</div>
+<img className={(styles['class-28']) + " class=bg1"} figma_id="1:36" src={a_f01a654c71c4e54e396fb7bbd74a102854be71df_jpg}/>
+<img className={(styles['class-29']) + " class=Bottom_logo"} figma_id="141:655" src={a_483874c40a6e2a7169ce83fd923590f7548a4fe1_jpg}/>
+</div>
+
+        </>
+    );
+};
+
+export default Loginpage;
