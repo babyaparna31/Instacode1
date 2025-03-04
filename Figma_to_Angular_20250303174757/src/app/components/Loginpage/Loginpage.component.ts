af66b382e149376f1c00b06a6425aeba36dec6e6
babyaparna31 <babyaparna31@gmail.com>
2025-03-04 19:52:27 +0530

    Delete Figma_to_Angular_20250303174757 directory

--- Figma_to_Angular_20250303174757/src/app/components/Loginpage/Loginpage.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-    @Component({
-        selector: 'app-Loginpage',
-        templateUrl: './Loginpage.component.html',
-        styleUrls: ['./Loginpage.component.scss']
-    })
-    export class LoginpageComponent {}
-    
