31dce29116d35f14af0f83f1085de478b5f5deda
babyaparna31 <babyaparna31@gmail.com>
2025-03-07 11:49:54 +0000

    Add/Update React project in dedicated folder

--- starter-master/src/app/pages/customer/customer.routing.ts
+++ starter-master/src/app/pages/customer/customer.routing.ts
@@ -1,17 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { NgModule } from '@angular/core';
-                    import { RouterModule, Routes } from '@angular/router';
-                    import { CustomerComponent } from './customer.component';
+import { CustomerComponent } from './customer.component';
 
-                    const routes: Routes = [
-                    {
-                        path: '',
-                        component: CustomerComponent
-                    }
-                    ];
+const routes: Routes = [
+  {
+    path: '',
+    component: CustomerComponent
+  }
+];
 
-                    @NgModule({
-                    imports: [RouterModule.forChild(routes)],
-                    exports: [RouterModule]
-                    })
-                    export class CustomerRoutingModule { }
-                    
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CustomerRoutingModule {}
