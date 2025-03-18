601aec30ff0c6f2dae920dab31aac73097e63a79
babyaparna31 <babyaparna31@gmail.com>
2025-03-18 18:22:49 +0530

    Delete InstaCode_Angular directory

--- InstaCode_Angular/src/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'mic-root',
-    template: '<router-outlet></router-outlet>',
-    standalone: false
-})
-export class AppComponent {}
